b6a9a9bff5447efcf5bb78e54f5312038d615463
Shivaram1704 <53655030+Shivaram17@users.noreply.github.com>
2022-12-27 00:23:30 +0530

    Create 61_RearrangeProducts.sql

---
+++ 61_RearrangeProducts.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+# SELECT product_id, store, price
+# FROM products
+# UNPIVOT(
+#     price
+#     FOR store IN (store1, store2, store3)
+# ) AS storepivot;
+SELECT product_id, 'store1' store, store1 price 
+FROM Products
+where store1 is not null
+union
+SELECT product_id, 'store2' store, store2 price 
+FROM Products
+where store2 is not null
+union
+SELECT product_id, 'store3' store, store3 price 
+FROM Products
+where store3 is not null;
